edc72f27d85daa24877b64fefb78edab95049ca8
BrotherDonkey <willbjorn@gmail.com>
2019-07-13 13:32:24 -0700

    add skull theme

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -6,7 +6,7 @@ bootstrapThemes();
 hljs.initHighlightingOnLoad();
 
 
-export type themes = 'fox' | 'lightning' | 'comet' | 'bluebird';
+export type themes = 'fox' | 'lightning' | 'comet' | 'skull';
 
 export function bootstrapThemes() {
     let defaultTheme = localStorage.getItem('liber-default-theme');
@@ -16,4 +16,21 @@ export function bootstrapThemes() {
     }
 
     document.documentElement.classList.add(`theme-${defaultTheme}`);
+
+    window.addEventListener('click', e => {
+        const target = e.target instanceof Element && e.target.closest('[data-theme-control]') as HTMLElement;
+        if (!target) {
+            return;
+        }
+        const theme = target.dataset.themeControl as themes;
+        if (theme) {
+            localStorage.setItem('liber-default-theme', theme);
+            removeAllThemeClass(document.documentElement);
+            document.documentElement.classList.add(`theme-${theme}`);
+        }
+    })
+}
+
+export function removeAllThemeClass(element: HTMLElement) {
+    element.classList.remove('theme-fox', 'theme-lightning', 'theme-skull', 'theme-comet');
 }
